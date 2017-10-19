929fb8fb9082809285622d572f1d64778b815d3e
wangrangzhi <451155928@qq.com>
2017-10-20 00:02:52 +0800

    add

--- 新手村-顺序和分支/1.cpp
+++ 新手村-顺序和分支/1.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main() {
     int a,b;
-    cin >> a >> b;
-    cout << a+b;
+   // cin >> a >> b;
+    cout << "aÍù\n";
+    cout << "aÍù";
     return 0;
 }
--- 新手村-顺序和分支/p1089.cpp
+++ 新手村-顺序和分支/p1089.cpp
@@ -1,29 +1,58 @@
-##include<iostream>
+#include<iostream>
 #include<cstdio>
 #include<cstdlib>
-
+using namespace std;
 int main()
 {
 	int a[12];
-	int save = 0, left;
+	int save = 0, left, monthLess = 0;
 	
 	for( int i = 0; i < 12; i++)
 		cin >> a[i];
 		
 	for(int i = 0; i<12; i++)
 	{
-		left = 300 - a[i];
-		if( (i == 0) && (left >= 100))
+		
+		if(i == 0) 
 		{
-			save += (left/100) * 100;
-			left = left%100;
+			left = 300 - a[i];
+			if(left < 0)
+			{
+				monthLess = 1;
+				break;
+			}
+			if(left >= 0)
+			{
+				save += (left/100) * 100;
+				left = left%100;
+			}
 		}
 		
 		if(i != 0)
 		{
-				
-		}	
+			left = 300 + left - a[i];
+			if(left < 0)
+			{
+				monthLess = i+1;
+				break;	
+			}
+			
+			if(left >= 0)
+			{
+				save += (left/100) * 100;
+				left = left%100;	
+			}	
+		}
+		
+		//cout << "left"<< left << "\n";	
+	//	cout << "save"<< save << "\n";	
 	}
 	
+	if(monthLess != 0)
+		cout << "-"<< monthLess;
+	if(monthLess == 0) 
+		cout << save ;
+		
+//	cout << "m" << monthLess <<"save"<<save ;
 	return 0;
 }
