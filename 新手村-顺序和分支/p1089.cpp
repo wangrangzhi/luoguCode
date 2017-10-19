55666655868129754192c7410fc8bec507707de0
wangrangzhi <451155928@qq.com>
2017-10-19 23:17:56 +0800

    add

--- 新手村-顺序和分支/p1089.cpp
+++ 新手村-顺序和分支/p1089.cpp
@@ -5,15 +5,24 @@
 int main()
 {
 	int a[12];
-	int saveTothemother, left;
+	int save = 0, left;
 	
 	for( int i = 0; i < 12; i++)
 		cin >> a[i];
 		
 	for(int i = 0; i<12; i++)
 	{
+		left = 300 - a[i];
+		if( (i == 0) && (left >= 100))
+		{
+			save += (left/100) * 100;
+			left = left%100;
+		}
 		
-		
+		if(i != 0)
+		{
+				
+		}	
 	}
 	
 	return 0;
