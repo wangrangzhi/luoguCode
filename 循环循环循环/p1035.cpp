abd456c457332e8c4017f0fb9512eb95354c9e38
wangrangzhi <451155928@qq.com>
2017-10-21 01:22:34 +0800

    add

---
+++ 循环循环循环/p1035.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+using namespace std;
+
+
+
+int main()
+{
+	int k = 0;
+	cin >> k ;
+	
+	double sn = 0.0000;
+	int n = 0;
+	
+	while( sn <= k)
+	{
+		++n;
+		sn += (double)1.0/n;
+		
+	}
+	
+	cout << n << endl;
+	
+//	cout << sn;
+	return 0;
+}
+
+
+
