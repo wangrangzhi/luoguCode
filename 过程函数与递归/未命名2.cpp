fdb92b853baf95f1fdb570f2caaa074834a014a6
wangrangzhi <451155928@qq.com>
2017-11-02 22:16:04 +0800

    add

--- 过程函数与递归/未命名2.cpp
+++ 过程函数与递归/未命名2.cpp
@@ -1,15 +1,28 @@
-#include<cstdio>
+#include<iostream>
+#include<math.h>
 using namespace std;
-int main(){
-    int n,cnt=1,i,f[1010];
-    f[0]=f[1]=1;
-    scanf("%d",&n);
-    for(i=2;i<=n;i++){
-        if(i%2==0){
-            f[i]=f[i-1]+f[i/2];
-        }else{
-            f[i]=f[i-1];
-        }
+int x[20],n,k;//依照题目所设
+bool isprime(int n){//判断是否质数
+    int s=sqrt(double(n));
+    for(int i=2;i<=s;i++){
+        if(n%i==0)return false;
     }
-    printf("%d\n",f[n]);
+    return true;
+}
+int rule(int choose_left_num,int already_sum,int start,int end){
+//choose_left_num为剩余的k，already_sum为前面累加的和，
+  //  start和end为全组合剩下数字的选取范围；调用递归生成全组合，
+ //   在过程中逐渐把K个数相加，当选取的数个数为0时，
+  //  直接返回前面的累加和是否为质数即可
+    if(choose_left_num==0)return isprime(already_sum);
+    int sum=0;
+    for(int i=start;i<=end;i++){
+        sum+=rule(choose_left_num-1,already_sum+x[i],i+1,end);
+    }
+    return sum;
+}
+int main(){
+    cin>>n>>k;
+    for(int i =0;i<n;i++)cin>>x[i];
+    cout<<rule(k,0,0,n-1);//调用递归解决问题
 }
