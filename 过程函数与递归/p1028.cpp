6635bc9e0a84589911bedf286baa3b7f09b55c92
wangrangzhi <451155928@qq.com>
2017-11-02 18:04:19 +0800

    add

---
+++ 过程函数与递归/p1028.cpp
@@ -0,0 +1,28 @@
+#include<cstdio>
+//理解题目 
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+
+
+int main()
+{
+	int n, cnt = 1, i, f[1010];
+
+	f[0] = f[1] = 1;
+
+	cin >> n;
+
+	for(int i = 2; i <= n; i++)
+	{
+		if(i%2 == 0)
+			f[i] = f[i-1] + f[i/2];
+		else
+			f[i] = f[i-1];
+	}
+
+	cout << f[n];
+
+	return 0;
+}
