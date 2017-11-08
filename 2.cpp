043afb803ca546284012ab5517ccbd18b5e38b37
wangrangzhi <451155928@qq.com>
2017-11-08 23:33:15 +0800

    add

---
+++ 2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int n, ans1; ans2[10001][11], sum, a[11];
+
+void trys(int t, int m)
+{
+	if(t>10)
+	{
+		if(m == n)
+		{
+			ans1++;
+			for(int i = 1; i<=10; i++)
+				ans2[ans1][i] = a[i];
+		}
+		return ;
+	}
+	for(int i = 1; i<=3; i++)
+	{
+		if(m+i>n)
+			break;
+
+		a[t] = i;
+		trys(t+1, m+i);
+		a[t] = 0;
+	}
+}
+
+int main()
+{
+	cin >> n;
+	trys(1, 0);
+	cout << ans1<<endl;
+
+	for(int i = 1; i<=ans1; i++)
+	{
+		
+	}
+
+	return 0;
+}
