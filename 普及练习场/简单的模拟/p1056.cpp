9a7d49be5af1d1116ee7a7fe88c9057c03c7cf43
wangrangzhi <451155928@qq.com>
2017-11-11 22:20:34 +0800

    add

--- 普及练习场/简单的模拟/p1056.cpp
+++ 普及练习场/简单的模拟/p1056.cpp
@@ -8,19 +8,18 @@ struct pairBettween {
 	int qi;
 } ;
 
-struct showTime
-{
+struct showTime {
 	int line;
 	int showCheck;
 
 };
 
 bool moreCheck(const showTime &lhs, const showTime &rhs) {
-    return lhs.showCheck > rhs.showCheck;
+	return lhs.showCheck > rhs.showCheck;
 }
 
 bool underTheLine(const showTime &lhs, const showTime &rhs) {
-    return lhs.line < rhs.line;
+	return lhs.line < rhs.line;
 }
 
 int main() {
@@ -37,10 +36,21 @@ int main() {
 
 	showTime cross[M];
 	int crossInt = 0;
-	memset(cross, 0, M);
+
+	for(int i=0; i<M; i++)
+	{
+		cross[i].line = -1;
+		cross[i].showCheck = 0;
+	}
+
+
 	showTime vertical[N];
 	int verticalInt = 0;
-	memset(vertical, 0, N);
+	for(int i=0; i<N; i++)
+	{
+		vertical[i].line = -1;
+		vertical[i].showCheck = 0;
+	}
 
 
 	if( K == 0 && L == 0) {
@@ -51,17 +61,16 @@ int main() {
 
 	for(int i = 0; i<D; i++) {
 		if(pairStu[i].xi == pairStu[i].pi) {
-			
+
 			int save1 = pairStu[i].yi < pairStu[i].qi ? pairStu[i].yi : pairStu[i].qi;
-			
+
 			for(int j = 0; j<N; j++) {
-				if(vertical[j].line == save1)
-				{
+				if(vertical[j].line == save1) {
 					Exist = true;
-					vertical[j].showCheck++;	
+					vertical[j].showCheck++;
 				}
 			}
-			
+
 			if(!Exist) {
 				vertical[verticalInt].line = save1;
 				verticalInt++;
@@ -70,15 +79,14 @@ int main() {
 		}
 
 		if(pairStu[i].yi == pairStu[i].qi) {
-			
+
 			int save2 = pairStu[i].xi < pairStu[i].pi ? pairStu[i].xi : pairStu[i].pi;
 
-			for(int j = 0; j<K; j++) {
-				if(cross[j].line == save2)
-				{
+			for(int j = 0; j<M; j++) {
+				if(cross[j].line == save2) {
 					Exist = true;
-					cross[j].showCheck++;	
-					
+					cross[j].showCheck++;
+
 				}
 			}
 
@@ -91,25 +99,66 @@ int main() {
 		Exist = false;
 	}
 
-//	stable_sort(cross, cross+K);
-	stable_sort(vertical, vertical+ sizeof(vertical)/sizeof(showTime), moreCheck);
-	
-	cout << "_______________________________\n";
 
-//	stable_sort(vertical, vertical+ sizeof(showTime)*L, underTheLine);
 
-	for(int i = 0 ; i<L; i++) {
+		int showCro[K];
 	
+	stable_sort(cross, cross+ sizeof(cross)/sizeof(showTime), moreCheck);
+	
+	for(int i = 0 ; i<K; i++) {
+	
+	//	cout <<"line" << cross[i].line << " " << "showCheck"<<  cross[i].showCheck  << "----\n";
 		
-		
-		cout << vertical[i].line << " " << ;
+		showCro[i] = cross[i].line;
+	
 	}
+	
+	sort(showCro, showCro+K);
+	
+	for(int i = 0; i<K; i++)
+		cout << showCro[i] << " ";
+
 
 	cout << "\n";
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//	stable_sort(cross, cross+K);
+	stable_sort(vertical, vertical+ sizeof(vertical)/sizeof(showTime), moreCheck);
+
+//	cout << "_______________________________\n";
+
+//	stable_sort(vertical, vertical+ sizeof(showTime)*L, underTheLine);
+
+	int showVer[L];
+
 	for(int i = 0 ; i<L; i++) {
-	//	cout << vertical[i] << " ";
+	//	cout <<"line" << vertical[i].line << " " << "showCheck"<<  vertical[i].showCheck  << "----\n";
+		
+		showVer[i] = vertical[i].line;
 	}
+	
+	sort(showVer, showVer+L);
+	
+	for(int i = 0; i<L; i++)
+		cout << showVer[i] << " ";
+
+//	cout <<"L" << L << "\n" <<sizeof(vertical)/sizeof(showTime) << "\n";
+
+
+
+	
 
 	return 0;
 }
