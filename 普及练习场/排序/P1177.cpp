f4edc105b31c84ea2503ee5604c8734811bd5265
wangrangzhi <451155928@qq.com>
2018-04-27 15:41:36 +0800

    排序

--- 普及练习场/排序/P1177.cpp
+++ 普及练习场/排序/P1177.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <algorithm>
+using namespace std;
 
 int a[100005];
 
@@ -12,4 +13,10 @@ int main()
     sort(a, a+n);
 
     printf("%d", a[0]);
+
+    for(int i=1; i<n; printf(" %d", *(a+(i++))));
+
+    puts("");
+
+    return 0;
 }
